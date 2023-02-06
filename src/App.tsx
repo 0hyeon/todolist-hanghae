3190c998188618ae1f06dfd00d74286bb83676b1
0hyeon <djdjdjk2006@naver.com>
2023-02-07 02:10:26 +0900

    feat:todo create기능추가

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,38 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  const [isTitle, setTitle] = useState<String>("");
-  const [isContent, setContent] = useState<String>("");
-  const handleSubmit = (isTitle: String, isContent: String) => {
-    alert("hello");
+  const [isTitle, setTitle] = useState<string>("");
+  const [isContent, setContent] = useState<string>("");
+
+  const [users, setUsers] = useState([
+    {
+      id: 1,
+      title: "리액트 공부하기",
+      content: "리액트 기초를 공부해봅시다.",
+      done: true,
+    },
+    {
+      id: 2,
+      title: "리액트 공부하기",
+      content: "리액트 기초를 공부해봅시다.",
+      done: false,
+    },
+  ]);
+  const titleChangeHandler = (e: any) => {
+    setTitle(e.target.value);
+  };
+  const contentChangeHandler = (e: any) => {
+    setContent(e.target.value);
+  };
+  const handleSubmit = () => {
+    const newUser = {
+      id: users.length + 1,
+      title: isTitle,
+      content: isContent,
+      done: false,
+    };
+
+    setUsers([...users, newUser]);
   };
   return (
     <div className="App">
@@ -27,27 +55,36 @@ function App() {
           <div>My Todo List</div>
           <div>React</div>
         </div>
-        <form
-          className="add-form"
-          onSubmit={(isTitle, isContent) => handleSubmit(isTitle, isContent)}
-        >
+        <form className="add-form" onSubmit={() => handleSubmit()}>
           <div className="input-group">
             <label className="form-label">제목</label>
             <input
               type="text"
               name="title"
               className="add-input input-body"
-              value=""
+              value={isTitle}
+              onChange={titleChangeHandler}
             />
             <label className="form-label">내용</label>
-            <input type="text" name="body" className="add-input" value="" />
+            <input
+              type="text"
+              name="body"
+              className="add-input"
+              value={isContent}
+              onChange={contentChangeHandler}
+            />
           </div>
-          <button className="add-button">추가하기</button>
+          <button className="add-button" onClick={handleSubmit}>
+            추가하기
+          </button>
         </form>
         <div className="list-container">
           <h2 className="list-title">Working.. 🔥</h2>
           <div className="list-wrapper">
             <div className="todo-container">
+              {/* {users.map((el: any, index) => (
+                <div key={index}>{el}</div>
+              ))} */}
               <div>
                 <h2 className="todo-title">리액트 공부하기</h2>
                 <div>리액트 기초를 공부해봅시다.</div>
