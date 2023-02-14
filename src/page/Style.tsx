ea2dada5e77a869c2c3fc7af644390352a299483
0hyeon <djdjdjk2006@naver.com>
2023-02-14 10:09:47 +0900

    style: 버튼컴포넌트,셀렉트 제작

--- src/page/Style.tsx
+++ src/page/Style.tsx
@@ -5,9 +5,25 @@ function Style() {
   const [isName, setName] = useState<string>("");
   const [isNumber, setNumber] = useState<string>("");
   const changeEnteredNum = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const value: string = e.target.value;
-    const removedCommaValue: number = Number(value.replaceAll(",", ""));
-    setNumber(removedCommaValue.toLocaleString());
+    let value = e.target.value; // 입력값을 value 라고 선언
+    const numCheck = /^[0-9,]/.test(value); // 입력값이 숫자와 콤마(,)인지 확인 (불린값이 나옴)
+
+    if (!numCheck && value) return; // 숫자가 아닌 문자로 이루어져 있으면 pass! (입력이 x)
+
+    if (numCheck) {
+      // 숫자이면
+      const numValue = value.replaceAll(",", ""); // 잠시 콤마를 때주고
+      value = numValue.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ","); // 받은 값에 3자리수마다 콤마를 추가
+    }
+    setNumber(value); // 바깥에서 사용할 수 있도록 state 값에 세팅해주자
+  };
+  const submitClcik = (isName: string, isNumber: string) => {
+    if (isName === "" || isNumber === "") {
+      alert("이름과 가격 모두 입력해주세요.");
+      return;
+    }
+    let pureNum = isNumber.split(",").join("");
+    alert(`{ name: ${isName}, price: ${pureNum}}`);
   };
   return (
     <div>
@@ -19,11 +35,12 @@ function Style() {
             fontWeight="600"
             bgColor="#fff"
             border="3px solid #55EFC4"
+            height="50px"
             onClick={() => {
               alert("버튼을 만들어보세요");
             }}
           >
-            Large Primary Button
+            Large Negative Button
           </Button>
           <Button width="130px" bgColor="#55EFC4">
             Medium
@@ -35,9 +52,11 @@ function Style() {
         <ButtonWrap>
           <Button
             width="200px"
+            height="50px"
             fontWeight="600"
             bgColor="#fff"
             border="3px solid #FAB1A0"
+            color="#D63031"
             onClick={() => {
               prompt("어렵나요?");
             }}
@@ -65,18 +84,61 @@ function Style() {
             }
           />
           가격 :
-          <Input type="number" value={isNumber} onChange={changeEnteredNum} />
+          <Input type="text" value={isNumber} onChange={changeEnteredNum} />
+          <Button
+            width="100px"
+            height="40px"
+            fontWeight="600"
+            bgColor="#55EFC4"
+            onClick={() => {
+              submitClcik(isName, isNumber);
+            }}
+          >
+            저장
+          </Button>
         </ButtonWrap>
       </TopWrapper>
-      <H2>Modal</H2>
-      <H2>Select</H2>
+      <TopWrapper>
+        <H2>Modal</H2>
+        <ButtonWrap>
+          <Button width="100px" height="40px" bgColor="#55EFC4">
+            open modal
+          </Button>
+          <Button
+            width="200px"
+            height="50px"
+            fontWeight="600"
+            bgColor="#fff"
+            border="3px solid #FAB1A0"
+            color="#D63031"
+            onClick={() => {
+              prompt("어렵나요?");
+            }}
+          >
+            open modal
+          </Button>
+        </ButtonWrap>
+      </TopWrapper>
+      <TopWrapper border="3px solid #ddd" marginTop="50px">
+        <H2>Select</H2>
+        <ButtonWrap>
+          <select name="" id="">
+            <option value="">리액트</option>
+            <option value="">자바</option>
+            <option value="">스프링</option>
+            <option value="">리액트네이티브</option>
+          </select>
+        </ButtonWrap>
+      </TopWrapper>
     </div>
   );
 }
-const TopWrapper = styled.div`
+const TopWrapper = styled.div<{ border?: string; marginTop?: string }>`
   display: flex;
   flex-direction: column;
   gap: 10px;
+  border: ${(props) => (props.border ? props.border : null)};
+  margin-top: ${(props) => (props.marginTop ? props.marginTop : null)};
 `;
 const H2 = styled.div`
   display: block;
@@ -93,13 +155,14 @@ const Button = styled.button<{
   color?: string;
   border?: string;
   fontWeight?: string;
+  height?: string;
 }>`
   border: ${(props) => (props.border ? props.border : null)};
   cursor: pointer;
   border-radius: 8px;
   background-color: ${(props) => (props.bgColor ? props.bgColor : null)};
   color: ${(props) => (props.color ? props.color : "#000")};
-  height: 45px;
+  height: ${(props) => (props.height ? props.height : "45px")};
   padding: 1px 6px;
 
   font-weight: ${(props) => (props.fontWeight ? props.fontWeight : null)};
